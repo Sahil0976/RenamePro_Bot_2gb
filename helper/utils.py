36b53563c52f0ecb4cfdf397e30e994e93de41fe
Nozel Silva <97865856+Sahil0976@users.noreply.github.com>
2023-11-04 11:02:20 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -77,7 +77,7 @@ async def send_log(b, u):
         await b.send_message(
             Config.LOG_CHANNEL,
             f"**--Nᴇᴡ Uꜱᴇʀ Sᴛᴀʀᴛᴇᴅ Tʜᴇ Bᴏᴛ--**\n\nUꜱᴇʀ: {u.mention}\nIᴅ: `{u.id}`\nUɴ: @{u.username}\n\nDᴀᴛᴇ: {date}\nTɪᴍᴇ: {time}\n\nBy: {b.mention}"
-        )
+    )
         
 
 
