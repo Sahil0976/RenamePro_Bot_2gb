430a6ed315e500dde1993dacc914fc2cb35ae0b6
Nozel Silva <97865856+Sahil0976@users.noreply.github.com>
2023-11-04 15:51:04 +0530

    Update Force_Sub.py

--- plugins/Force_Sub.py
+++ plugins/Force_Sub.py
@@ -26,6 +26,7 @@ async def forces_sub(client, message):
         [InlineKeyboardButton(text="❤️ 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋​ ❤️", url=f"https://t.me/Its_Tartaglia_Childe")],
         [InlineKeyboardButton(text="📢 Join Update Channel 📢", url=f"https://t.me/{Config.FORCE_SUB}") ]
             ]
+    )
     text = "**Sᴏʀʀy Dᴜᴅᴇ Yᴏᴜ'ʀᴇ Nᴏᴛ Jᴏɪɴᴇᴅ My Cʜᴀɴɴᴇʟ 😐. Sᴏ Pʟᴇᴀꜱᴇ Jᴏɪɴ Oᴜʀ Uᴩᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ Tᴏ Cᴄᴏɴᴛɪɴᴜᴇ**"
     try:
         user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)    
